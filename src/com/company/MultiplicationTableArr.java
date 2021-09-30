1cfabaf6e6a8edc7610c5f977d1f8be0dd4d15d6
Burlakov Denys <denis055023@gmail.com>
2021-09-30 21:21:37 +0300

    MultiplicationTableArr secondvariant

--- src/com/company/MultiplicationTableArr.java
+++ src/com/company/MultiplicationTableArr.java
@@ -26,15 +26,11 @@ public class MultiplicationTableArr {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 MULTIPLICATION_TABLE[i - 1][j - 1] = i * j;
-            }
-        }
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
                 System.out.print(MULTIPLICATION_TABLE[i-1][j-1] + " ");
-
             }
             System.out.println();
         }
+
     }
 
 }
